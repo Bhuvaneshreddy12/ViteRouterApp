b24d8970c48b6f6740fb5ac0b34b4c34a934246f
Bhuvanesh Reddy <bhuvanredy12@gmail.com>
2025-07-24 17:56:48 +0530

    intial app.jsx setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,33 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-import { Routes,Route} from 'react-router-dom'
+import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from 'react-router-dom'
 import Home from './pages/Home'
 import Products from './pages/Products'
 import About from './pages/About'
-import Contact from './pages/Contact'
+
+import RootLayout from './layout/RootLayout'
+import Contactinfo from './components/Contactinfo'
+import ContactForm from './components/ContactForm'
+import ContactLayout from './layout/ContactLayout'
 
 
 const App = () => {
+  const router=createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<RootLayout/>}>
+      <Route index element={<Home/>}/>
+      <Route path='/products' element={<Products/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/contact' element={<ContactLayout/>}>
+        
+        <Route path='info' element={<Contactinfo/>}/>
+        <Route path='form' element={<ContactForm/>}/>
+      </Route>
+      </Route>
+    )
+  )
   return (
-    <div>
-    <Navbar/>
-    <div className='container'>
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/Products' element={<Products/>}/>
-      <Route path='/About' element={<About/>}/>
-      <Route path='/Contact' element={<Contact/>}/>
-
-    </Routes>
-    </div>
-    </div>
+   <RouterProvider router={router}/>
   )
 }
 
