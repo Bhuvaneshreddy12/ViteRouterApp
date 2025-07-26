f34a75a4b7ccb21de2459931e150d0dfa3329f70
Bhuvanesh Reddy <bhuvanredy12@gmail.com>
2025-07-26 12:15:47 +0530

    Creating API and fetching job details from data.json file and adding css to the job details

--- src/pages/Jobs.jsx
+++ src/pages/Jobs.jsx
@@ -1,16 +1,27 @@
 import React from 'react'
+import { Link, useLoaderData } from 'react-router-dom'
 
 const Jobs = () => {
+    const jobsData=useLoaderData();
   return (
-    <div>
-      
+    <div  className='jobs'>
+      {jobsData.map((job)=>{
+            return <Link>
+                <h4>{job.title}</h4>
+                <h4>{job.location}</h4>
+            </Link>
+
+      })}
     </div>
   )
 }
 
 export default Jobs
 export const jobsLoader=async ()=>{
-    
+    const res=await fetch("http://localhost:5000/jobs");
+    return res.json();
+
+
 }
 
 
